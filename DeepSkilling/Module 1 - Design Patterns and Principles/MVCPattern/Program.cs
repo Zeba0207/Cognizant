083e7ff263c1816c052341ddc9e6059caad6de78
Zeba0207 <shaikzebafathima6@gmail.com>
2026-06-23 18:57:23 +0530

    Add MVC Pattern project

---
+++ DeepSkilling/Module 1 - Design Patterns and Principles/MVCPattern/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace MVCPattern
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student = new Student(101, "Zeba", "A");
+
+            StudentView view = new StudentView();
+
+            StudentController controller =
+                new StudentController(student, view);
+
+            controller.UpdateView();
+
+            controller.SetStudentName("Fathima");
+            controller.SetStudentGrade("A+");
+
+            System.Console.WriteLine("\nAfter Update:\n");
+
+            controller.UpdateView();
+        }
+    }
+}
